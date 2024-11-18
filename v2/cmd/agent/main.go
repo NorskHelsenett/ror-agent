0a6a746d4aa8d499b7f38b1fd01f17e6ecf6939e
Håvard Elnan <havard.elnan@nhn.no>
2024-11-18 12:36:44 +0100

    rebuild2

--- v2/cmd/agent/main.go
+++ v2/cmd/agent/main.go
@@ -24,7 +24,7 @@ import (
 )
 
 func main() {
-	_ = "rebuild 7"
+	_ = "rebuild 8"
 	_, _ = maxprocs.Set(maxprocs.Logger(rlog.Infof))
 	agentconfig.Init()
 	rlog.Info("Agent is starting", rlog.String("version", viper.GetString(configconsts.VERSION)), rlog.String("commit", viper.GetString(configconsts.COMMIT)))

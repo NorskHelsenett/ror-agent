3dcbd3986f9173ae56f7122a6cee7e2b24f94b41
Håvard Elnan <havard.elnan@nhn.no>
2025-05-19 20:27:47 +0200

    fix version handling

--- internal/config/variables.go
+++ internal/config/variables.go
@@ -3,27 +3,34 @@ package config
 import (
 	"github.com/NorskHelsenett/ror/pkg/config/configconsts"
 
+	"github.com/NorskHelsenett/ror/pkg/config/rorversion"
 	"github.com/NorskHelsenett/ror/pkg/rlog"
 
 	"github.com/spf13/viper"
 )
 
+const (
+	VersionDefault = "0.1.2"
+	CommitDefault  = "FFFFF"
+)
+
 var (
-	version    string = "1.1.0"
-	commit     string = "FFFFF"
+	version    = rorversion.NewRorVersion(VersionDefault, CommitDefault)
 	ErrorCount int
 )
 
 func Init() {
 	rlog.InitializeRlog()
 	rlog.Info("Configuration initializing ...")
-	viper.SetDefault(configconsts.VERSION, version)
-	viper.SetDefault(configconsts.COMMIT, commit)
+	viper.SetDefault(configconsts.VERSION, VersionDefault)
+	viper.SetDefault(configconsts.COMMIT, CommitDefault)
 	viper.SetDefault(configconsts.HEALTH_ENDPOINT, ":8100")
 	viper.SetDefault(configconsts.POD_NAMESPACE, "ror")
 	viper.SetDefault(configconsts.API_KEY_SECRET, "ror-apikey")
-
 	viper.AutomaticEnv()
+	version = rorversion.NewRorVersion(viper.GetString(configconsts.VERSION), viper.GetString(configconsts.COMMIT))
+	viper.Set(configconsts.VERSION, version.Version)
+	viper.Set(configconsts.COMMIT, version.Commit)
 }
 
 func IncreaseErrorCount() {
@@ -31,4 +38,8 @@ func IncreaseErrorCount() {
 }
 func ResetErrorCount() {
 	ErrorCount = 0
+}
+
+func GetRorVersion() rorversion.RorVersion {
+	return version
 }

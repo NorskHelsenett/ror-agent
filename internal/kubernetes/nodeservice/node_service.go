b2b3fb0d7cadd93fee789eeedc2d0a9784dabdc2
Håvard Elnan <35875060+havardelnan@users.noreply.github.com>
2025-10-16 00:25:49 +0200

    Update node_service.go

--- internal/kubernetes/nodeservice/node_service.go
+++ internal/kubernetes/nodeservice/node_service.go
@@ -49,7 +49,7 @@ func GetNodes(k8sClient *kubernetes.Clientset, metricsClient *metrics.Clientset)
 		n.Name = node.Name
 		n.Labels = node.Labels
 
-		switch report.GetProvider() {
+		switch report.report.Interregator {
 		case providermodels.ProviderTypeTanzu:
 			fillNodeTanzu(&n)
 		case providermodels.ProviderTypeAks:

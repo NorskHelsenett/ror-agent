0ee9e17b85c4a939c5434696ea5332e2fce64958
Håvard Elnan <35875060+havardelnan@users.noreply.github.com>
2026-03-12 17:44:06 +0100

    feat: Implement cluster agent client and dynamic client handler (#173)
    - Added `clusteragentclient` package to manage interactions with the ror-agent API.
    - Introduced `dynamicclient` package to handle dynamic resource watching and event handling.
    - Created `agentconsts` package for environment variable constants related to dynamic watching.
    - Developed `dynamiccontroller` package to manage dynamic resource events with caching options.
    - Implemented health and pprof services for monitoring and profiling capabilities.

---
+++ common/pkg/config/agentconsts/agentconst.go
@@ -0,0 +1,7 @@
+package agentconsts
+
+const (
+	DynamicWatchNoCacheEnv                 = "ROR_DYNAMIC_WATCH_NO_CACHE"
+	ForceGCAfterInitialListEnv             = "ROR_FORCE_GC_AFTER_INITIAL_LIST"
+	ForceGCAfterInitialListFreeOSMemoryEnv = "ROR_FORCE_GC_AFTER_INITIAL_LIST_FREE_OS_MEMORY"
+)

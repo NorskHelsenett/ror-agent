47cc83ade360f0e11f6ab6246655fe5638fa7ece
Håvard Elnan <havard.elnan@nhn.no>
2026-03-12 17:52:14 +0100

    Refactor code structure for improved readability and maintainability

--- pkg/config/agentconsts/agentconst.go
+++
@@ -1,7 +0,0 @@
-package agentconsts
-
-const (
-	DynamicWatchNoCacheEnv                 = "ROR_DYNAMIC_WATCH_NO_CACHE"
-	ForceGCAfterInitialListEnv             = "ROR_FORCE_GC_AFTER_INITIAL_LIST"
-	ForceGCAfterInitialListFreeOSMemoryEnv = "ROR_FORCE_GC_AFTER_INITIAL_LIST_FREE_OS_MEMORY"
-)
